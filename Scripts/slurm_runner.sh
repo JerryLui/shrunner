1faec2106495d817b2fc81d7455cd6f315812095
Liu, Jerry X <jerry.x.liu@aptiv.com>
2018-04-04 14:41:56 +0200

    Working version

--- Scripts/slurm_runner.sh
+++ Scripts/slurm_runner.sh
@@ -1,10 +1,17 @@
 #!/bin/bash -l
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 #SBATCH --time=15:00
 #SBATCH -mem=4096
 #SBATCH -A VGTT
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+# Author: Jerry Liu		(jerry.x.liu@aptiv.com)
+# General Script Runner
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 
-PATH_SCRIPT = $1
-PATH_FILE = $2
+PATH_SCRIPT=$1
+PATH_FILE=$2
+
+module load mcr/8.1
 
 exec ${PATH_SCRIPT} ${PATH_FILE} &
 
